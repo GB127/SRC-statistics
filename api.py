008f7f4ef740ec2abeb528d36218a5171a6d4fe8
Guylain <guylain.breton.1@ulaval.ca>
2020-06-13 16:15:21 -0400

    Now I have the times of my runs!

--- api.py
+++ api.py
@@ -1,6 +1,25 @@
 import requests
 import matplotlib
+import datetime
+import isodate
+
+URL = "https://www.speedrun.com/api/v1"
+
+def getruns(username):
+    rep = requests.get(f"{URL}/users/niamek/personal-bests")
+    if rep.status_code == 200:
+        rep = rep.json()
+    return rep["data"]
+        # This returns my list of PBs : 1 dicto per run
+            # place : my rank
+            # run :
+                # Game
+                # Category
+                # Status
+                # Times (OK)
+            # category: ???
 
 
-
-
+runs = getruns("niamek")
+for i in runs :
+    print(isodate.parse_duration(i["run"]["times"]["primary"]))
