c4279f1c08da033f4c4545e7944499dd3e4fb2d0
Guylain <guylain.breton.1@ulaval.ca>
2020-06-13 17:43:15 -0400

    more stuffs

--- api.py
+++ api.py
@@ -5,21 +5,32 @@ import isodate
 
 URL = "https://www.speedrun.com/api/v1"
 
-def getruns(username):
-    rep = requests.get(f"{URL}/users/niamek/personal-bests")
+def get_userID(username):
+    rep = requests.get(f"{URL}/users/{username}")
     if rep.status_code == 200:
         rep = rep.json()
-    return rep["data"]
-        # This returns my list of PBs : 1 dicto per run
-            # place : my rank
-            # run :
-                # Game
-                # Category
-                # Status
-                # Times (OK)
-            # category: ???
+    return rep["data"]["id"]
 
 
-runs = getruns("niamek")
-for i in runs :
-    print(isodate.parse_duration(i["run"]["times"]["primary"]))
+def getruns(username, all=False):
+
+    if not all:
+        rep = requests.get(f"{URL}/users/{username}/personal-bests")
+        if rep.status_code == 200:
+            rep = rep.json()
+        return rep["data"]
+            # This returns my list of PBs : 1 dicto per run
+                # place : my rank
+                # run :
+                    # Game
+                    # Category
+                    # Status
+                    # Times (OK)
+    elif all:
+        rep = requests.get(f"{URL}/runs?user=x7qz6qq8")
+        if rep.status_code == 200:
+            rep = rep.json()
+        return rep["data"]
+
+test = get_userID("niamek")
+print(test)
