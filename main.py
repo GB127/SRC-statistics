1a2b66c12ddb5a225e68f879c91ba09da00f6c1b
Guylain <guylain.breton.1@ulaval.ca>
2020-06-26 16:40:21 -0400

    Fix #6 : Now we have an interface

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from classe import *
 from api import *
 
-test = runner("niamek")
-test.plot()
+
+if __name__ == "__main__":
+    print("Fetching data, please wait...")
+    user = runner("niamek")
+    #user = runner(input("Which user name would you like to see? "))
+    print("Fetching complete!")
+    use = True
+    while use:
+        command = input("What would you like to do? ")
+        if command == "plot":
+            user.plot()
+        if command == "exit":
+            use = False
+    print("Program terminated")
