f4d68aeb1fa40f11b74f1a024b8e3d498da04093
Guylain <guylain.breton.1@ulaval.ca>
2020-07-23 09:01:55 -0400

    Massive updates

--- api.py
+++ api.py
@@ -6,15 +6,39 @@ import isodate
 
 URL = "https://www.speedrun.com/api/v1"
 
+def get_runs(username):
+    runs = []
+    unwanted = ["weblink", "videos", "comment", "status","links","values", "id", "level", "players", "date", "submitted", "splits"]
+    link_1 = f"{URL}/runs?user={get_userID(username)}&max=200"
+
+    def getter(link):
+        print(link)
+        rep = requests.get(link)
+        if rep.status_code == 200:
+            rep = rep.json()
+        for run in rep["data"]:
+            for unwant in unwanted: del run[unwant]
+            run["times"] = run["times"]["primary"]
+            runs.append(run)
+        if rep["pagination"]["size"] == rep["pagination"]["max"]:
+            print("hallo")
+            getter(rep["pagination"]["links"][-1]["uri"])
+        if rep["pagination"]["size"] < rep["pagination"]["max"]:
+            pass
+    
+    getter(link_1)
+    return runs
+
+
+
+
 def get_userID(username):
     rep = requests.get(f"{URL}/users/{username}")
     if rep.status_code == 200:
         rep = rep.json()
         return rep["data"]["id"]
 
-
 games = {}
-
 def get_game(ID):
     # ID or acronym
     try:
@@ -26,10 +50,37 @@ def get_game(ID):
             games[ID] = rep["data"]["names"]["international"]
     return games[ID]
 
+categories = {}
+def get_category(ID):
+    try:
+        return categories[ID]
+    except KeyError:
+        rep = requests.get(f'{URL}/categories/{ID}')
+        if rep.status_code == 200:
+            rep = rep.json()
+            categories[ID] = rep["data"]["name"]
+    return categories[ID]
+
+WRs = {}
+def get_WR(ID):
+    try:
+        return WRs[ID]
+    except KeyError:
+        WRs[ID] = []
+        rep = requests.get(f'{URL}/categories/{ID}/records')
+        if rep.status_code == 200:
+            rep = rep.json()
+            for place in rep["data"][0]["runs"]:
+                WRs[ID].append(place["run"]["times"]["primary"])
+            WRs[ID].sort()
+    return WRs[ID]
+
 def get_leaderboard():
     # TODO: Doublons à éliminer quand possible!
+    # TODO: Séparer les onglets
+    # TODO: Game Argument & Category argument
     ranking = []
-    rep = requests.get(f"{URL}/leaderboards/sm64/category/16_Star")
+    rep = requests.get(f"{URL}/leaderboards/sml2/category/Any_Glitchless")
     if rep.status_code == 200:
         rep= rep.json()
 
@@ -41,5 +92,4 @@ def get_leaderboard():
 
 
 if __name__ == "__main__":
-    print(get_game("sms"))
-    print(games)
+    print(get_runs("niamek"))
