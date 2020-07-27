2d54e0bc8f404236494664c1346c98bfd451aec7
Guylain <guylain.breton.1@ulaval.ca>
2020-07-27 09:01:36 -0400

    Massive update

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-joimport requests
+import requests
 import matplotlib
 import datetime
 import isodate
@@ -14,8 +14,10 @@ def get_runs(username):
         rep = requests.get(link)
         if rep.status_code == 200:
             rep = rep.json()
+        else: raise BaseException(rep.status_code)
         for run in rep["data"]:
-            runs.append(run)
+            if run["level"] is None: pass
+            else: runs.append(run)
         if rep["pagination"]["size"] == rep["pagination"]["max"]:
             getter(rep["pagination"]["links"][-1]["uri"])
         if rep["pagination"]["size"] < rep["pagination"]["max"]:
@@ -60,37 +62,102 @@ def get_category(ID):
     return categories[ID]
 
 WRs = {}
+
+
 def get_WR(ID):
     try:
         return WRs[ID]
     except KeyError:
         WRs[ID] = []
-        rep = requests.get(f'{URL}/categories/{ID}/records')
+        rep = requests.get(f'{URL}/categories/{ID}/records?top=1')
         if rep.status_code == 200:
             rep = rep.json()
             for place in rep["data"][0]["runs"]:
                 WRs[ID].append(place["run"]["times"]["primary"])
             WRs[ID].sort()
+        else:
+            raise BaseException
     return WRs[ID]
 
 
 leaderboard = {}
 def get_len_leaderboard(gameID, categID):
-    pass
+    try:
+        return len(leaderboard[(gameID, categID)])
+    except KeyError:
+        ranking = []
+        rep = requests.get(f"{URL}/leaderboards/{gameID}/category/{categID}")
+        if rep.status_code == 200:
+            rep= rep.json()
+            for run in rep["data"]["runs"]:
+                ranking.append((int(run["place"]), isodate.parse_duration(run["run"]["times"]["primary"]).total_seconds()))
+            leaderboard[(gameID, categID, variable)] = ranking
+        else:
+            raise BaseException(rep.status_code)
+    return len(leaderboard[(gameID, categID, variable)])
+
 def get_leaderboard(gameID, categID):
     # TODO: Doublons à éliminer quand possible!
     # TODO: Séparer les onglets
-    ranking = []
-    rep = requests.get(f"{URL}/leaderboards/{gameID}/category/{categID}")
+    try:
+        return leaderboard[(gameID, categID)]
+    except KeyError:
+        ranking = []
+        rep = requests.get(f"{URL}/leaderboards/{gameID}/category/{categID}")
+        if rep.status_code == 200:
+            rep= rep.json()
+            for run in rep["data"]["runs"]:
+                ranking.append((int(run["place"]), isodate.parse_duration(run["run"]["times"]["primary"]).total_seconds()))
+            leaderboard[(gameID, categID)] = ranking
+        else:
+            raise BaseException(rep.status_code)
+    return leaderboard[(gameID, categID)]
+
+systems = {
+    None : "PC",
+    "n5683oev" : "GB",
+    "gde3g9k1" : "GBC",
+    "3167d6q2" : "GBA",
+    "w89rwelk" : "N64",
+    "jm95z9ol" : "NES",
+    "3167jd6q" : "SGB",
+    "83exk6l5" : "SNES",
+    "nzelreqp" : "WII VC"}
+def get_newsystem(newsystem):
+    rep = requests.get(f"{URL}/platforms?max=200")
     if rep.status_code == 200:
         rep= rep.json()
-
-        for run in rep["data"]["runs"]:
-            ranking.append((int(run["place"]), isodate.parse_duration(run["run"]["times"]["primary"]).total_seconds()))
+        for system in rep["data"]:
+            if system["name"] == newsystem: 
+                print("-------------------------------------")
+                print(f'"{system["id"]}" : "{system["name"]}",')
+                print("-------------------------------------")
+                return
+        for system in rep["data"]:
+            print(system["name"])
     else:
         raise BaseException(rep.status_code)
-    return ranking
+
+
+def get_system(ID):
+    try:
+        return systems[ID]
+    except KeyError:
+        rep = requests.get(f"{URL}/platforms/{ID}")
+        if rep.status_code == 200:
+            rep= rep.json()
+            systems[ID] = rep["data"]["name"]
+        else:
+            raise BaseException(rep.status_code)
+    return systems[ID]
 
 
 if __name__ == "__main__":
-    get_PBs("niamek")
+    
+    
+    game = "o1y9wo6q"
+    cat = "7dgrrxk4"
+
+    test = get_leaderboard(game, cat)
+
+    print(test)
--- classe.py
+++ classe.py
@@ -8,13 +8,20 @@ from tools import *
 class user:
     def __init__(self, username):
         self.username = username
+        print("initializing data...")
         self.ID = get_userID(self.username)
         self.runs, self.PBs = [], []
         for run in get_runs(self.ID): self.runs.append(Run(run))
-        for pb in get_PBs(self.ID): self.PBs.append(PB(pb))
+        for pb in get_PBs(self.ID): 
+            if pb["run"]["level"] is None:
+                self.PBs.append(PB(pb))
+            else: pass
+        print("user initialized!")
     def printruns(self):
         for run in self.runs: print(run)
     def listPBs(self):
+        print(f"|{'Sys':^6}| {'Game':^30}| {'Category':^15} | {'  Rank      (^%)':20}| Time     (+ \u0394WR)")
+        print("-"*120)
         for PB in self.PBs: print(PB)
 class Run:
     def __init__(self, data):
@@ -23,28 +30,27 @@ class Run:
         self.ID = data["id"]
         self.gameID = data["game"]
         self.categID = data["category"]
+        self.variID = data["values"]
         self.time = isodate.parse_duration(data["times"]["primary"]).total_seconds()
     def __str__(self):
         return f'{get_game(self.gameID)} - {get_category(self.categID)} - {datetime.timedelta(seconds=self.time)}'
 
 class PB(Run):
     def __init__(self, data):
-        self.place = data["place"]
         super().__init__(data["run"])
+        self.place = data["place"]
         self.lenrank = get_len_leaderboard(self.gameID, self.categID)
         self.WR = isodate.parse_duration(get_WR(self.categID)[0]).total_seconds()
     def __str__(self):
         def str_game(self):
-            return f'{get_game(self.gameID)[:30]:30}|{get_category(self.categID):20}'
+            return f'|{get_system(self.system)[:6]:^6}| {get_game(self.gameID)[:30]:30}| {get_category(self.categID)[:15]:15}'
         def str_rank(self):
             calculation = f'{self.place}/{self.lenrank}'
-            calculation2 = "(" + str(round(100 * (self.lenrank - self.place) / self.lenrank,2)) + " %)"
-            return str(f'{calculation:^9} {calculation2:9}')
+            calculation2 = f'({str(round(100 * (self.lenrank - self.place) / self.lenrank,2)):^5} %)'
+            return str(f'{calculation:^9} {calculation2}')
         def str_times(self):
-            return f'{str_time(self.time):9}(+ {str_time(self.time - self.WR)})'
-        return f'{str_game(self)}|{str_rank(self)}|{str_times(self)}'
-
-
+            return f'{str_time(self.time):9}(+ {str_time(self.time - self.WR)[:11]:11})'
+        return f'{str_game(self)} | {str_rank(self)} | {str_times(self)}'
 
 
 if __name__ == "__main__":
