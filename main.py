eb9428f9cf3e9d6b6e8cd4ecb063c5ecb8efd122
Guylain <guylain.breton.1@ulaval.ca>
2020-08-07 11:10:17 -0400

    Some main changes

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ from plots import *
 from classe import *
 
 if __name__ == "__main__":
-    user = user(input("who? "))
+    #user = user(input("who? "))
+    user = user("niamek")
     user.table_PBs()
-    command = True
+    command = True  
     while command != "end":
-        command = input("[lb]")
+        command = input("[lb, runs, PBs] ")
         if command == "PBs": user.table_PBs()
+        if command == "runs":
+            which = int(input("Please enter a number")) -1
+            plot_runs(user.PBs[which].gameID, 
+                      user.PBs[which].categID,
+                      user.PBs[which].WR,
+                      user.runs)
         if command == "lb":
             which = int(input("Please enter a number")) -1
             plot_leaderboard(user.PBs[which].gameID,
                             user.PBs[which].categID,
                             PB=user.PBs[which],
-                            username=user.username, vari=user.PBs[which].vari)
-
+                            username=user.username,
+                            vari=user.PBs[which].vari)
     print("Script ended, thank you!")
