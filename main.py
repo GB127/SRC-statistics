af9c97b60a74f39a6eee143108675597ee0bff3a
Guylain <guylain.breton.1@ulaval.ca>
2020-08-26 11:36:56 -0400

    Some stuffs!

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ import os
 clear = lambda: os.system('cls')
 
 def run_analyser(user):
+    clear()
+    user.table_PBs()
     which = int(input(f"Enter a number from 1 to {len(user.PBs)} ")) -1
     PB = user.PBs[which]
 
     plot_leaderboard(PB, user.username)
     plot_runs(PB, user.username, user.runs_PB(PB) )
 
-def user_analyser(user):
+def system_analyser(user):
+    clear()
+    user.table_systems()
 
 
-
-
-    plot_user_pies_systems(user)
+    plot_systems(user)
 
 
     # Emu vs. non-emu
@@ -34,14 +36,16 @@ def input_which(length):
         print("allo this is a test")
 
 if __name__ == "__main__":
-    user = user(input("Who? "))
+    #user = user(input("Who? "))
+    user = user("baffan")
     main = True
     while main:
         clear()
-        user.table_PBs()
-        command = input("[all, 1, end] ")
-        if command == "all":
-            user_analyser(user)
-        elif command == "1":
+        command = input("[system, run, sort end] ")
+        if command == "system":
+            system_analyser(user)
+        elif command == "run":
             run_analyser(user)
+        elif command == "sort":
+            PB.sort = input("Which sorting method? [game, system, time, delta, %WR, %LB] ")
         elif command == "end": main = False
