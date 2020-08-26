c989af9e4d99948f0ca35c6d6698868f29b45a82
Guylain <guylain.breton.1@ulaval.ca>
2020-08-26 13:01:00 -0400

    Now input can accept 'all'

--- main.py
+++ main.py
@@ -7,39 +7,38 @@ clear = lambda: os.system('cls')
 def run_analyser(user):
     clear()
     user.table_PBs()
-    which = int(input(f"Enter a number from 1 to {len(user.PBs)} ")) -1
-    PB = user.PBs[which]
+    which = input_which(user.PBs)
+    if which == "all":
+        pass
+    else:
+        PB = user.PBs[which]
 
-    plot_leaderboard(PB, user.username)
-    plot_runs(PB, user.username, user.runs_PB(PB) )
+        plot_leaderboard(PB, user.username)
+        plot_runs(PB, user.username, user.runs_PB(PB) )
 
 def system_analyser(user):
     clear()
     user.table_systems()
-
-
-    plot_systems(user)
-
-
-    # Emu vs. non-emu
-
+    which = input_which(user.all_systems)
+    if which == "all":
+        plot_systems(user)
+    else:
+        plot_system(user, user.all_systems[which])
 
     # pie chart of WR difference per system
     # pie chart of WR% per system
-    # Histogram as well
-    # Histogram as well
 
 def input_which(length):
+    command = input(f"[1 - {len(length)}, all] ")
     try:
-        return int(input(f"Enter a number from 1 to {len(length)}"))
+        return int(command) - 1
     except ValueError:
-        print("allo this is a test")
+        return command
 
 if __name__ == "__main__":
     user = user(input("Who? "))
     main = True
     while main:
-        clear()
         command = input("[system, run, sort end] ")
         if command == "system":
             system_analyser(user)
