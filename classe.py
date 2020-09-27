b04e8291fc663df636e1067e02ea4f309c4a0037
Guylain <guylain.breton.1@ulaval.ca>
2020-09-27 15:48:45 -0400

    Move plot leaderboards to class.py leaderboard class

--- classe.py
+++ classe.py
@@ -533,6 +533,27 @@ class leaderboard:
         pass
 
 
+    def plot_leaderboard(leaderboards):
+
+        for year in leaderboards.keys():
+            if len(leaderboards[year]) == 1:
+                plot.plot([time[0] for time in leaderboards[year]], [rank[1] for rank in leaderboards[year]], label=year, marker="*")
+            else:
+                plot.plot([time[0] for time in leaderboards[year]], [rank[1] for rank in leaderboards[year]], label=year)
+
+        plot.ylabel("Time")
+        plot.yticks(plot.yticks()[0],[datetime.timedelta(seconds=x) for x in plot.yticks()[0]])
+
+        plot.xlabel("Rank")
+        ax = plot.gca()
+        ax.set_xlim(ax.get_xlim()[::-1])
+        
+
+
+        plot.show()
+
+
+
 
 
 
