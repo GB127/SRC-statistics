fd0a24f18f76faeb04a3353a8b35ff2b871b7831
Guylain <guylain.breton.1@ulaval.ca>
2021-02-23 00:12:29 -0500

    Reduce clutterness of the code

--- api.py
+++ api.py
@@ -5,98 +5,9 @@ import time
 
 URL = "https://www.speedrun.com/api/v1"
 
-def recursive_requester(link, toupdate):
-    """Recursive requester.
-
-        Args:
-            link (string): Complete link of the request to do. 
-            toupdate (list): list to update with all the recursive requests.
-                Should be an empty list as the goal of this function is to "return" a list.
-
-        Returns nothing, but modify the "toupdate" list that can then be used elsewhere.
-        """
-    rep = requests.get(link)
-    if rep.status_code == 200:
-        rep = rep.json()
-    for run in rep["data"]:
-        toupdate.append(run)
-    if rep["pagination"]["size"] == rep["pagination"]["max"]:
-        recursive_requester(rep["pagination"]["links"][-1]["uri"], toupdate)
-    if rep["pagination"]["size"] < rep["pagination"]["max"]:
-        pass
-
-
-def requester(link):
-    """Generic requester.
-
-        Args:
-            link (string): link addition of the request
-
-        Raises:
-            BaseException: Raise error if the status code is 404.
-
-        Returns:
-            json: return the data in a json form
-        """
-    while True:
-        # It's in a loop in order to bypass the 502 status code.
-        rep = requests.get(f"{URL}{link}")
-        if rep.status_code == 200:
-            return rep.json()
-        elif rep.status_code == 502:
-            time.sleep(10)
-        elif rep.status_code == 404:
-            print(rep.status_code)
-            print(f"{URL}{link}")
-            raise BaseException("Incorrect info, please check again")
-        else:
-            raise BaseException(f"Please report this, {rep.status_code} - {URL}{link}")
-
-def get_PBs(ID):
-    """Requests the PBs of the said username
-
-        Args:
-            username (str): username
-
-        Returns:
-            data
-        """
-    rep = requester(f"/users/{ID}/personal-bests")
-    return rep["data"]
-
-def get_userID(username):
-    """Get the user ID of a username.
-
-        Args:
-            username (str): username
-
-        Returns:
-            ID (str)
-    """
-    rep = requester(f"/users/{username}")
-    return rep["data"]["id"]
-
-def get_game(ID):
-    """Fetch the full name of the game with an ID or acronym(str).
-        """
-    rep = requester(f"/games/{ID}")
-    return rep["data"]["names"]["international"]
-
-def get_category(ID):
-    """Fetch the full name of the category with an ID.
-        Returns (str): Full name of the category
-        """
-    rep = requester(f'/categories/{ID}')
-    return rep["data"]["name"]
 
 def get_WR(gameID, categID, vari):
-    """[summary]
-
-    Args:
-        gameID ([type]): [description]
-        categID ([type]): [description]
-        vari ([type]): [description]
-
+    """
     Returns:
         [type]: [description]
     """
@@ -210,17 +121,17 @@ def get_leaderboards(gameID, categID, vari):
             rankings[year] = tempo
     return rankings
 
+########### Below this are function that works. No need of improving.
+
 
 def get_newsystem(newsystem):
     """Debug function to find a new system to give a new acronym in the dictionnary.
 
         Args:
-            newsystem (str): string to "acronymize". Need to be exactly the same.
+            newsystem (str): string to "acronymize". Need to be exact.
 
         Result : print([ID] : "Full name")
-
-        If no system matches with the newsystem seeked, it will print all available systems.
-        Find the system you want to acronymize and rerun the code with the corrections.
+            NOTE: If no system matches, print all available systems.
 
         """
     rep = requester(f"/platforms?max=200")
@@ -233,6 +144,77 @@ def get_newsystem(newsystem):
     for system in rep["data"]:
         print(system["name"])
 
+
+def recursive_requester(link, toupdate):
+    """Recursive requester with the provided link.
+
+        Args:
+            link (string): Complete link of the request to do. 
+            toupdate (list): list to update with all the recursive requests.
+                Should be an empty list as the goal of this function is to "return" a list.
+
+        Returns nothing, but modify the "toupdate" list that can then be used elsewhere.
+        """
+    rep = requests.get(link)
+    if rep.status_code == 200:
+        rep = rep.json()
+    for run in rep["data"]:
+        toupdate.append(run)
+    if rep["pagination"]["size"] == rep["pagination"]["max"]:
+        recursive_requester(rep["pagination"]["links"][-1]["uri"], toupdate)
+    if rep["pagination"]["size"] < rep["pagination"]["max"]:
+        pass
+
+
+def requester(link):
+    """Generic requester that request data with the link provided.
+        Raises: BaseException: Raise error if bad data is given in the link.
+        Returns : data in a json form
+        """
+    while True:
+        # It's in a loop in order to bypass the 502 status code.
+        rep = requests.get(f"{URL}{link}")
+        if rep.status_code == 200:
+            return rep.json()
+        elif rep.status_code == 502:
+            time.sleep(10)
+        elif rep.status_code == 404:
+            print(rep.status_code)
+            print(f"{URL}{link}")
+            raise BaseException("Incorrect info, please check again")
+        else:
+            raise BaseException(f"Please report this, {rep.status_code} - {URL}{link}")
+
+
+def get_PBs(ID):
+    """Requests the PBs of the said username identified with the ID.
+        Returns: data (json format)
+        """
+    rep = requester(f"/users/{ID}/personal-bests")
+    return rep["data"]
+
+def get_userID(username):
+    """Get the user ID of a username.
+        Returns:
+            ID (str)
+        """
+    rep = requester(f"/users/{username}")
+    return rep["data"]["id"]
+
+def get_game(ID):
+    """Fetch the full name of the game with an ID or acronym(str).
+        """
+    rep = requester(f"/games/{ID}")
+    return rep["data"]["names"]["international"]
+
+def get_category(ID):
+    """Fetch the full name of the category with an ID.
+        Returns (str): Full name of the category
+        """
+    rep = requester(f'/categories/{ID}')
+    return rep["data"]["name"]
+
+
 def get_system(ID):
     """Return the system.
         Returns: (str) full name of the system.
@@ -240,14 +222,9 @@ def get_system(ID):
     rep = requester(f"/platforms/{ID}")
     return rep["data"]["name"]
 
-
-
 def get_runs(ID):
     """Returns a list of all the runs (json form) by the user 
         identified with the ID provided. Uses the recursive requester.
-
-        Args:
-            ID (string) : ID of the user to fetch runs from.
 
         Returns (list) : list of all runs of the user (still in json data form).
     """
