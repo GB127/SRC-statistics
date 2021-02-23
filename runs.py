482c8011cac86bbc63a6b36b5620c4e7ac9e5be8
Guylain <guylain.breton.1@ulaval.ca>
2021-02-22 23:40:07 -0500

    Small commit

--- runs.py
+++ runs.py
@@ -21,4 +21,4 @@ class PB:
         self.category = get_category(data_1["category"])
 
     def __str__(self):
-        return f'{self.game}{self.category}{self.time}'
+        return f'{self.game}{self.category}{self.time}{self.place}'
