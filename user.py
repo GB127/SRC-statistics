8ff0dc252f955720c26469208f167c3345f87b39
Guylain <guylain.breton.1@ulaval.ca>
2021-02-24 09:49:12 -0500

    Combine runs and pbs table, and allow for future similar tables

--- user.py
+++ user.py
@@ -24,7 +24,7 @@ class user:
                     run.leaderboard = pb.leaderboard
         print("user initialized!")
 
-    def table_Runs(self):
+    def table_data(self, data):
         def entete():
             tempo = [f'{"#":^3}',
                 f'{"System":^7}',
@@ -37,49 +37,21 @@ class user:
         def pied():
             tempo = [f'{len(self.Runs):^3}',
                 f'{"Total":>53}',
-                f'{self.Runs.total_time():>9}',
-                f'+ {self.Runs.total_deltaWR():<9}']
+                f'{data.total_time():>9}',
+                f'+ {data.total_deltaWR():<9}']
             print("-" * ((len(" | ".join(tempo)) + 3)))
             print( " | ".join(tempo))
             tempo = [
                 f'{"Mean":>59}',
-                f'{self.Runs.mean_time():>9}',
-                f'+ {self.Runs.mean_deltaWR():<9}']
+                f'{data.mean_time():>9}',
+                f'+ {data.mean_deltaWR():<9}']
             print( " | ".join(tempo))
         entete()
-        for no, run in enumerate(self.Runs):
-            print(f'{no+1:^3} | {run}')
-        pied()
-
-
-    def table_PBs(self):
-        def entete():
-            tempo = [f'{"#":^3}',
-                f'{"System":^7}',
-                f'{"Game":20}',
-                f'{"Category":20}',
-                f'{"Time":^9}',
-                f'{"deltaWR"}']  # NOTE : Put back the delta symbol
-            print( " | ".join(tempo))
-            print("-" * ((len(" | ".join(tempo)) + 3)))
-        def pied():
-            tempo = [f'{len(self.PBs):^3}',
-                f'{"Total":>53}',
-                f'{self.PBs.total_time():>9}',
-                f'+ {self.PBs.total_deltaWR():<9}']
-            print("-" * ((len(" | ".join(tempo)) + 3)))
-            print( " | ".join(tempo))
-            tempo = [
-                f'{"Mean":>50}',
-                f'{self.PBs.mean_time():>9}',
-                f'+ {self.PBs.mean_deltaWR():<9}']
-            print( " | ".join(tempo))
-        entete()
-        for no, run in enumerate(self.PBs):
-            print(f'{no+1:^3} | {run}')
+        for no, entry in enumerate(data):
+            print(f'{no+1:^3} | {entry}')
         pied()
 
 
 if __name__ == "__main__":
     test = user("niamek")
-    test.table_Runs()
+    test.table_data(test.PBs)
