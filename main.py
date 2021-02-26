31aa6e75f4cce32b6732c7e59c49dd85733d16a6
Guylain <guylain.breton.1@ulaval.ca>
2021-02-26 00:59:45 -0500

    command slecter + start main.py

--- main.py
+++ main.py
@@ -3,15 +3,30 @@ import os
 clear = lambda: os.system('cls')
 
 
+def command_select(iterable, printer=True):
+    while True:
+        try:
+            commant = input(f"Which option? [1 - {len(iterable)}]")
+            if int(commant) != 0:
+                return iterable[int(commant) - 1]
+            raise BaseException
+        except:
+            if commant != "end":
+                pass
+            else:
+                return "end"
+
 if __name__ == "__main__":
     clear()
     print("speedrun.com statistics fetcher, program written by Niamek")
-    #user = user(input("Which sr user? "))
     user = user("niamek")
     commands = []
     for attribute in user.__dict__.values():
         if not isinstance(attribute, str):
             commands.append(attribute)
-
-
-    commands[1]()
+    while True:
+        selection = command_select(commands)
+        if selection == "end":
+            break
+        else:
+            selection()
