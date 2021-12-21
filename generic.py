1484ed762d12295c42eeff59f0523c8fd4855099
Guylain <guylain.breton.1@ulaval.ca>
2021-12-21 11:59:07 -0500

    Start subcategory

--- generic.py
+++ generic.py
@@ -74,6 +74,7 @@ class Entry:
 
         if not level:
             del self.level
+        del self.date
 
         for attribute, repertoire, funct_req in zip(
                 ["game",        "category",         "system"], 
@@ -86,23 +87,30 @@ class Entry:
                 repertoire[data[attribute]] = funct_req(data[attribute])
                 self.__dict__[attribute] = repertoire[data[attribute]]
 
+        def fix_subcateg():
+            for category, subcat in self.values.items():
+                données = get_variable(category)
+                if données["mandatory"]:
+                    self.category += f" {données['values']['values'][subcat]['label']} "
+            del self.values
+
+        fix_subcateg()
+
     def __str__(self):
         time_str = lambda x : f'{int(x)//3600:02}:{int(x) % 3600 // 60:02}:{int(x) % 3600 % 60 % 60:02}'
 
         stringed = []
         for attribu, value in self.__dict__.items():
-            print(attribu)
             try:
                 if "time" not in attribu:
                     stringed.append(f'{str(value)[:spacing[attribu]]:{spacing[attribu]}}')
                 else:
-                    stringed.append(f'{time_str(value)[:spacing["time"]]:{spacing["time"]}}')
-
+                    stringed.append(f'{time_str(value):{spacing["time"]}}')
             except KeyError:
                 stringed.append(f'{str(value)[:10]:10}')
         return " | ".join(stringed)
 
 if __name__ == "__main__":
-    entry_data = {'id': 'z073gloy','place': 14 , 'weblink': 'https://www.speedrun.com/bhero/run/z073gloy', 'game': 'nd2eeqd0', 'level': None, 'category': 'zd3yzr2n', 'videos': {'links': [{'uri': 'https://www.twitch.tv/videos/1110770410'}]}, 'comment': 'Blind race. Stellar hitboxes right here.', 'status': {'status': 'verified', 'examiner': '98rpeqj1', 'verify-date': '2021-08-08T19:00:00Z'}, 'players': [{'rel': 'user', 'id': 'x7qz6qq8', 'uri': 'https://www.speedrun.com/api/v1/users/x7qz6qq8'}], 'date': '2021-08-06', 'submitted': '2021-08-07T05:35:16Z', 'times': {'primary': 'PT4H2M40S', 'primary_t': 14560, 'realtime': 'PT4H2M40S', 'realtime_t': 14560, 'realtime_noloads': None, 'realtime_noloads_t': 0, 'ingame': None, 'ingame_t': 0}, 'system': {'platform': 'nzelreqp', 'emulated': False, 'region': 'pr184lqn'}, 'splits': None, 'values': {}, 'links': [{'rel': 'self', 'uri': 'https://www.speedrun.com/api/v1/runs/z073gloy'}, {'rel': 'game', 'uri': 'https://www.speedrun.com/api/v1/games/nd2eeqd0'}, {'rel': 'category', 'uri': 'https://www.speedrun.com/api/v1/categories/zd3yzr2n'}, {'rel': 'platform', 'uri': 'https://www.speedrun.com/api/v1/platforms/nzelreqp'}, {'rel': 'region', 'uri': 'https://www.speedrun.com/api/v1/regions/pr184lqn'}, {'rel': 'examiner', 'uri': 'https://www.speedrun.com/api/v1/users/98rpeqj1'}]}
+    entry_data = {'id': 'z073gloy','place': 14 , 'weblink': 'https://www.speedrun.com/bhero/run/z073gloy', 'game': 'nd2eeqd0', 'level': None, 'category': 'zd3yzr2n', 'videos': {'links': [{'uri': 'https://www.twitch.tv/videos/1110770410'}]}, 'comment': 'Blind race. Stellar hitboxes right here.', 'status': {'status': 'verified', 'examiner': '98rpeqj1', 'verify-date': '2021-08-08T19:00:00Z'}, 'players': [{'rel': 'user', 'id': 'x7qz6qq8', 'uri': 'https://www.speedrun.com/api/v1/users/x7qz6qq8'}], 'date': '2021-08-06', 'submitted': '2021-08-07T05:35:16Z', 'times': 14560, 'system': {'platform': 'nzelreqp', 'emulated': False, 'region': 'pr184lqn'}, 'splits': None, 'values': {}, 'links': [{'rel': 'self', 'uri': 'https://www.speedrun.com/api/v1/runs/z073gloy'}, {'rel': 'game', 'uri': 'https://www.speedrun.com/api/v1/games/nd2eeqd0'}, {'rel': 'category', 'uri': 'https://www.speedrun.com/api/v1/categories/zd3yzr2n'}, {'rel': 'platform', 'uri': 'https://www.speedrun.com/api/v1/platforms/nzelreqp'}, {'rel': 'region', 'uri': 'https://www.speedrun.com/api/v1/regions/pr184lqn'}, {'rel': 'examiner', 'uri': 'https://www.speedrun.com/api/v1/users/98rpeqj1'}]}
     test_class = Entry(entry_data)
     print(test_class)
