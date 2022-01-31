8347824dc72c60998eeecf3e4223aa9103816611
Guylain <guylain.breton.1@ulaval.ca>
2022-01-30 20:29:36 -0500

    Update the code

--- code_SRC/run_entry.py
+++ code_SRC/run_entry.py
@@ -1,6 +1,6 @@
 from .api import api
 
-#from api import api
+# from api import api
 from copy import copy
 
 
@@ -9,13 +9,16 @@ class Run:
         self.__dict__ = data
         self["game"] = api.game(self["game"])
         self["category"] = api.category(self["category"])
+        self["emu"] = self["system"]["emulated"]
+        self["region"] = self["system"]["region"]
         self["system"] = api.system(self["system"]["platform"])
         self["time"] = self["times"]["primary_t"]
+
         for unwanted in ["id", "weblink", "videos","level", "values", "submitted", "splits", "links", "comment", "times", "players", "status"]:
             del self.__dict__[unwanted]
 
     def __str__(self):
-        string = " ".join([str(self[x]) for x in ["game", "time", "system"]])
+        string = " ! ".join([str(self[x]) for x in ["system", "game", "time"]])
         return string
 
     def __getitem__(self, key):
@@ -26,10 +29,15 @@ class Run:
 
     def __add__(self, other):
         copie = copy(self)
-        if other == 0:
+        if isinstance(other, int) and other == 0:
             return copie
         for attribute in self.__dict__:
-            copie[attribute] += other[attribute]
+            if isinstance(copie[attribute] , int):
+                copie[attribute] += other[attribute]
+            elif copie[attribute] == other[attribute]:
+                continue
+            elif not isinstance(copie[attribute], set):
+                copie[attribute] = {copie[attribute], other[attribute]}
         return copie
 
 
@@ -42,5 +50,8 @@ class Run:
 
 if __name__== "__main__":
     run_base = {'id': 'z073gloy', 'weblink': 'https://www.speedrun.com/bhero/run/z073gloy', 'game': 'nd2eeqd0', 'level': None, 'category': 'zd3yzr2n', 'videos': {'links': [{'uri': 'https://www.twitch.tv/videos/1110770410'}]}, 'comment': 'Blind race. Stellar hitboxes right here.', 'status': {'status': 'verified', 'examiner': '98rpeqj1', 'verify-date': '2021-08-08T19:00:00Z'}, 'players': [{'rel': 'user', 'id': 'x7qz6qq8', 'uri': 'https://www.speedrun.com/api/v1/users/x7qz6qq8'}], 'date': '2021-08-06', 'submitted': '2021-08-07T05:35:16Z', 'times': {'primary': 'PT4H2M40S', 'primary_t': 14560, 'realtime': 'PT4H2M40S', 'realtime_t': 14560, 'realtime_noloads': None, 'realtime_noloads_t': 0, 'ingame': None, 'ingame_t': 0}, 'system': {'platform': 'nzelreqp', 'emulated': False, 'region': 'pr184lqn'}, 'splits': None, 'values': {}, 'links': [{'rel': 'self', 'uri': 'https://www.speedrun.com/api/v1/runs/z073gloy'}, {'rel': 'game', 'uri': 'https://www.speedrun.com/api/v1/games/nd2eeqd0'}, {'rel': 'category', 'uri': 'https://www.speedrun.com/api/v1/categories/zd3yzr2n'}, {'rel': 'platform', 'uri': 'https://www.speedrun.com/api/v1/platforms/nzelreqp'}, {'rel': 'region', 'uri': 'https://www.speedrun.com/api/v1/regions/pr184lqn'}, {'rel': 'examiner', 'uri': 'https://www.speedrun.com/api/v1/users/98rpeqj1'}]}
-    test = Run(run_base)
-    print(test)
+    test = Run(copy(run_base))
+    test2 = Run(copy(run_base))
+    test3 = test + test2
+    print(test.__dict__)
+    print(test3.__dict__)
