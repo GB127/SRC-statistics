8c54578d20f6a42d602af2df4da550f138d8ac76
Guylain <guylain.breton.1@ulaval.ca>
2022-01-30 19:50:27 -0500

    update code

--- code_SRC/api.py
+++ code_SRC/api.py
@@ -2,29 +2,48 @@ from requests import get
 
 class api:
     URL = "https://www.speedrun.com/api/v1/"
-    tempo = {}
+    game_db = {}
+    system_db = {}
+    category_db = {}
+    level_db = {}
 
     @staticmethod
     def game(id:str) -> str:
-        req = get(f'{api.URL}games/{id}').json()
         try:
-            return req["data"]["names"]["international"]
-        except:
-            raise BaseException(id)
+            return api.game_db[id]
+        except KeyError:
+            req = get(f'{api.URL}games/{id}').json()
+            api.game_db[id] = req["data"]["names"]["international"]
+        return api.game_db[id]
 
-    def system(id) -> str:
-        req = get(f'{api.URL}platforms/{id}').json()
-        return req["data"]["name"]
+    @staticmethod
+    def system(id:str) -> str:
+        try:
+            return api.system_db[id]
+        except KeyError:
+            req = get(f'{api.URL}platforms/{id}').json()
+            api.system_db[id] = req["data"]["name"]
+        return api.system_db[id]
 
 
     @staticmethod
-    def category(id) -> str:
-        req = get(f'{api.URL}categories/{id}').json()
-        return req["data"]["name"]
+    def category(id:str) -> str:
+        try:
+            return api.category_db[id]
+        except KeyError:
+            req = get(f'{api.URL}categories/{id}').json()
+            api.category_db[id] = req["data"]["name"]
+        return api.category_db[id]
 
+    @staticmethod
     def level(id) -> str:
-        req = get(f'{api.URL}levels/{id}').json()
-        return req["data"]["name"]
+        try:
+            return api.level_db[id]
+        except KeyError:
+            req = get(f'{api.URL}levels/{id}').json()
+            api.level_db[id] = req["data"]["name"]
+        return api.level_db[id]
+
 
     @staticmethod
     def user_id(username) -> str:
@@ -43,3 +62,4 @@ class api:
 if __name__ == "__main__":
     api.game("v1pxjz68")
     print(api.game("v1pxjz68"))
+    print(api.game_db)
--- code_SRC/run_entry.py
+++ code_SRC/run_entry.py
@@ -1,6 +1,7 @@
 from .api import api
 
-# from api import api
+#from api import api
+from copy import copy
 
 
 class Run:
@@ -10,11 +11,12 @@ class Run:
         self["category"] = api.category(self["category"])
         self["system"] = api.system(self["system"]["platform"])
         self["time"] = self["times"]["primary_t"]
-        for unwanted in ["id", "weblink", "videos", "submitted", "splits", "links", "comment", "times", "players", "status"]:
+        for unwanted in ["id", "weblink", "videos","level", "values", "submitted", "splits", "links", "comment", "times", "players", "status"]:
             del self.__dict__[unwanted]
 
     def __str__(self):
-        return self["game"]
+        string = " ".join([str(self[x]) for x in ["game", "time", "system"]])
+        return string
 
     def __getitem__(self, key):
         return self.__dict__[key]
@@ -22,6 +24,17 @@ class Run:
     def __setitem__(self, key, value):
         self.__dict__[key] = value
 
+    def __add__(self, other):
+        copie = copy(self)
+        if other == 0:
+            return copie
+        for attribute in self.__dict__:
+            copie[attribute] += other[attribute]
+        return copie
+
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
 
     def __lt__(self, other):
         return self.time < other.time
@@ -30,4 +43,4 @@ class Run:
 if __name__== "__main__":
     run_base = {'id': 'z073gloy', 'weblink': 'https://www.speedrun.com/bhero/run/z073gloy', 'game': 'nd2eeqd0', 'level': None, 'category': 'zd3yzr2n', 'videos': {'links': [{'uri': 'https://www.twitch.tv/videos/1110770410'}]}, 'comment': 'Blind race. Stellar hitboxes right here.', 'status': {'status': 'verified', 'examiner': '98rpeqj1', 'verify-date': '2021-08-08T19:00:00Z'}, 'players': [{'rel': 'user', 'id': 'x7qz6qq8', 'uri': 'https://www.speedrun.com/api/v1/users/x7qz6qq8'}], 'date': '2021-08-06', 'submitted': '2021-08-07T05:35:16Z', 'times': {'primary': 'PT4H2M40S', 'primary_t': 14560, 'realtime': 'PT4H2M40S', 'realtime_t': 14560, 'realtime_noloads': None, 'realtime_noloads_t': 0, 'ingame': None, 'ingame_t': 0}, 'system': {'platform': 'nzelreqp', 'emulated': False, 'region': 'pr184lqn'}, 'splits': None, 'values': {}, 'links': [{'rel': 'self', 'uri': 'https://www.speedrun.com/api/v1/runs/z073gloy'}, {'rel': 'game', 'uri': 'https://www.speedrun.com/api/v1/games/nd2eeqd0'}, {'rel': 'category', 'uri': 'https://www.speedrun.com/api/v1/categories/zd3yzr2n'}, {'rel': 'platform', 'uri': 'https://www.speedrun.com/api/v1/platforms/nzelreqp'}, {'rel': 'region', 'uri': 'https://www.speedrun.com/api/v1/regions/pr184lqn'}, {'rel': 'examiner', 'uri': 'https://www.speedrun.com/api/v1/users/98rpeqj1'}]}
     test = Run(run_base)
-    print(test["game"])
+    print(test)
