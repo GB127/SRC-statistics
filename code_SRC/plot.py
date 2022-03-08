12940ad02c5b3ecbd1882b48434db8510dafb048
Guylain <guylain.breton.1@ulaval.ca>
2022-03-08 12:45:29 -0500

    Functionnal app!

--- code_SRC/plot.py
+++ code_SRC/plot.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QComboBox, QWidget, QHBoxLayout, QVBoxLayout, QListWidget
+from PyQt5.QtWidgets import QGridLayout,QPushButton, QApplication,QComboBox, QWidget, QHBoxLayout, QVBoxLayout, QListWidget
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import matplotlib.pyplot as plt
 import matplotlib
@@ -13,63 +13,49 @@ class Histo_app(QWidget):
             for index, entry in enumerate(data_list):
                 self.listwidget.insertItem(index, str(entry))
             self.listwidget.clicked.connect(self.clicked)
-            layout.addWidget(self.listwidget)
+            layout.addWidget(self.listwidget, 0, 0, 0,1)
 
-        def insert_dropbox():
-            self.dropbox = QComboBox()
-            self.dropbox.setFixedWidth(300)
-            self.dropbox.addItems(["One", "Two", "Three"])
-            layout.addWidget(self.dropbox)
+        def insert_buttons():
+            for numéro, x in enumerate(data_list[0].keys()):
+                print(numéro, x)
+                dropbox = QPushButton(x)
+                dropbox.clicked.connect(lambda checked, a=x : self.update_chart(a))
+                layout.addWidget(dropbox, 1,1+numéro)
 
 
         def insert_plot():
+            self.canvas = FigureCanvas(plt.Figure(figsize=(15, 6)))
+            layout.addWidget(self.canvas, 0,1, 0, 1+ len(data_list[0].keys()))
             font = {'weight': 'normal',
                     'size': 16}
             matplotlib.rc('font', **font)
-            self.ax = self.canvas.figure.subplots()
-            self.ax.hist([x[self.filter] for x in data_list])
+            self.update_chart(random.choice(list(self.data[0].keys())))
 
-
-        self.filter = random.choice(list(data_list[0].keys()))
         super().__init__()
+        self.data = data_list
         self.setWindowTitle('Histogram!')
         self.window_width, self.window_height = 1200, 800
         self.setMinimumSize(self.window_width, self.window_height)
 
-
-        layout = QHBoxLayout()
+        layout = QGridLayout()
         self.setLayout(layout)
-
         create_list_widget()
-
-
-        self.canvas = FigureCanvas(plt.Figure(figsize=(15, 6)))
-
-        # Graphic zone
-        layout.addWidget(self.canvas)
-
         insert_plot()
+        insert_buttons()
 
-
-        insert_dropbox()
-
-    def clicked(self, qmodelindex):
+    def clicked(self):
         item = self.listwidget.currentItem()
         print(item.text())
 
 
-    def update_chart(self):
-        value = self.input.text()
-        try:
-            value = float(value)
-        except ValueError:
-            value = 0
+    def update_chart(self, y):
+        self.canvas.figure.clf()
+        self.ax = self.canvas.figure.subplots()
+        self.ax.hist([x[y] for x in self.data])
+        self.ax.set_title("allo")
+        self.ax.set_xlabel("XXXXXX")
+        self.ax.set_ylabel("YYYYYY")
 
-        x_position = [0.5]
-
-        if self.bar:
-            self.bar.remove()
-        self.bar = self.ax.bar(x_position, value, width=0.2, color='g')
         self.canvas.draw()
 
 
@@ -87,7 +73,7 @@ if __name__ == '__main__':
         }
     ''')
     
-    myApp = Histo_app([{"name":x, "allo": str(random.randint(0,10))} for x in range(30)])
+    myApp = Histo_app([{"patate":3, "macaroni":"lll", "name":x, "allo": str(random.randint(0,10))} for x in range(30)])
     myApp.show()
 
     # Need to keep this so it doesn't close the window
