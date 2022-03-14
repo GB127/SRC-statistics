9efb49b2f84af85a76316ebca89f3e544a27acd9
Guylain <guylain.breton.1@ulaval.ca>
2022-03-14 16:29:27 -0400

    start the loop works!

--- code_SRC/user.py
+++ code_SRC/user.py
@@ -1,6 +1,10 @@
 from code_SRC.api import api
 from tables.runs import Table_run
 from tables.pbs import Table_pb
+import os
+
+clear = lambda: os.system('cls')
+
 
 class User:
     def __init__(self, username):  # pragma: no cover
@@ -9,7 +13,18 @@ class User:
         self.runs = Table_run(api.user_runs(self.id))  # pragma: no cover
         self.pbs = Table_pb(api.user_pbs(self.id))  # pragma: no cover
 
+    def __call__(self):
+        while True:
+            clear()
+            print(self)
+            for index, fx in enumerate(["pbs", "runs"]):
+                print(index, fx)
+            command = input(f"Select option: [0-{2 -1}] | Type end to exit\nInput : ")
+            if command == "end":
+                break
+            self.__dict__[["pbs", "runs"][int(command)]]()
+
 
     def __str__(self):
         time_str = lambda x : f'{int(x)//3600:>3}:{int(x) % 3600 // 60:02}:{int(x) % 3600 % 60 % 60:02}'
-        return f"{self.username} | {len(self.runs.join('game'))} game | {len(self.runs.join('system'))} system\n{len(self.runs)} Runs ({time_str(self.runs.sum().time):>10})\n{len(self.pbs)} PBs  ({time_str(self.pbs.sum().time):>10})"
+        return f"{self.username} | {len(self.runs)} Runs ({time_str(self.runs.sum().time):>10})| {len(self.pbs)} PBs  ({time_str(self.pbs.sum().time):>10})"
---
+++ main.py
@@ -0,0 +1,7 @@
+from code_SRC.user import User
+
+
+test = User("libre")
+# print(test.pbs)
+
+test()
