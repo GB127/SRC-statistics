1e4c47b220d21c9489788f8c31e00aa1fccbf062
Guylain <guylain.breton.1@ulaval.ca>
2022-03-14 16:20:51 -0400

    small addition : keys

--- entries/base.py
+++ entries/base.py
@@ -4,6 +4,9 @@ class Base_Entry:
     def __getitem__(self, key):
         return self.__dict__[key]
 
+    def keys(self):
+        return self.__dict__.keys()
+
     def __setitem__(self, key, value):
         self.__dict__[key] = value
 
