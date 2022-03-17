4834210e750e7e12dc96c29f19749dc636edbfb6
Guylain <guylain.breton.1@ulaval.ca>
2022-03-17 13:32:11 -0400

    new unit test

--- tests/mocks.py
+++ tests/mocks.py
@@ -42,7 +42,16 @@ class PB_mock(PB):
 class Table_pb_mock(Table_pb):
     def __init__(self):
         self.data = [PB_mock() for x in range(5)]
+        for x in range(5):
+            self.data[x].time = 50 * x
+            self.data[x].game += str(x)
+            self.data[x]["WR %"] = 100 + (20 * x)
+            self.data[x].place = x
+
 
 class Table_run_mock(Table_run):
     def __init__(self):
         self.data = [Run_mock() for x in range(5)]
+        for x in range(5):
+            self.data[x].time = 50 * x
+            self.data[x].game += str(x)
--- tests/test_plots.py
+++ tests/test_plots.py
@@ -1,13 +1,64 @@
-from plots.handler import window_handler
 from plots.histo import Histo_app
-from tests.mocks import Table_run_mock
+from plots.pie import Pie_app
+from tests.mocks import Table_pb_mock, Table_run_mock
+from PyQt5 import QtCore
+
+class Test_entry_list:
+    def test_quantity(self, qtbot):
+        for histo in [Histo_app, Pie_app]:
+            widget = histo(Table_pb_mock().data)
+            qtbot.addWidget(widget)
+            assert len(widget.listwidget) == 5
+
+class Test_filters:
+    def test_quantity(self, qtbot):
+        # run : time
+        # PBs : time, place, WR %, WR time
+        for tableau, expected_len in [(Table_pb_mock, 4), (Table_run_mock, 1)]:
+            widget = Histo_app(tableau().data)
+            qtbot.addWidget(widget)
+            assert len(widget.buttons) == expected_len, f'{tableau.__name__} has {len(widget.buttons)} filter buttons, expected {expected_len}.'
+
+    def test_title(self, qtbot):
+        widget = Histo_app(Table_pb_mock().data)
+        qtbot.addWidget(widget)
+        qtbot.mouseClick(widget.buttons[0], QtCore.Qt.LeftButton)
+        assert widget.ax.get_title() == "PB_mock - place"
+
+class Test_histo:
+    def test_xticks(self, qtbot):
+        def place():
+            qtbot.mouseClick(widget.buttons[0], QtCore.Qt.LeftButton)
+            assert widget.ax.get_xlabel() == "place"
+            assert min(list(widget.ax.get_xticks())) > 0
+        def times():
+            for clé in [1,2]:
+                qtbot.mouseClick(widget.buttons[clé], QtCore.Qt.LeftButton)
+                assert widget.ax.get_xlabel() == "time"
+                assert min(list(widget.ax.get_xticks())) > 0
+                # assert False, widget.ax.get_xticklabels()
+                raise NotImplemented("Time string format check : TODO : Use regex to check format")
+        def percentage():
+            qtbot.mouseClick(widget.buttons[3], QtCore.LeftButton)
+            assert widget.ax.get_xlabel() == "WR %"
+            assert min(list(widget.ax.get_xticks())) >= 100
+
+        widget = Histo_app(Table_pb_mock().data)
+        qtbot.addWidget(widget)
+        place()
+        times()
+        percentage()
+
+    def test_yticks(self, qtbot):
+        widget = Histo_app(Table_pb_mock().data)
+        assert widget.ax.get_ylabel() == "Frequency"
+        assert min(widget.ax.get_yticks()) == 0
+        assert all([isinstance(x, int) for x in widget.ax.get_yticks()])
 
 
-def test_hello(qtbot):
-    widget = Histo_app(Table_run_mock().data)
-    qtbot.addWidget(widget)
-    assert False
-
+    #def test_bins(self, qtbot):
+    #    widget = Histo_app(Table_pb_mock().data)
+    #    assert False, widget.ax.containers[0][0]
 
     # click in the Greet button and make sure it updates the appropriate label
     #qtbot.mouseClick(widget.button_greet, QtCore.Qt.LeftButton)
