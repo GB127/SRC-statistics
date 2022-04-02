f88c2106ac7f0707b5166d78f26a5f2a142378b1
Guylain <guylain.breton.1@ulaval.ca>
2022-04-02 16:23:34 -0400

    docstring

--- entries/base_entry.py
+++ entries/base_entry.py
@@ -1,4 +1,13 @@
 class Base_Entry:
+    """Base entry. All common features from all the different
+        type of data handled by the program.
+        Methods:
+            __getitem__ / __setitem__
+            keys
+            __eq__
+        TODO : In child classes, Maybe try to inherit dictos instead of using a base entry?
+            if it works, this fille can be deleted.
+    """
     def __getitem__(self, key):
         return self.__dict__[key]
 
@@ -10,4 +19,3 @@ class Base_Entry:
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
-
--- entries/lb_entry.py
+++ entries/lb_entry.py
@@ -1,30 +1,49 @@
 from entries.one_run import Run
 
+
 class Rank(Run):
-    def __init__(self, data:dict, WR_time:int):
+    """Class related to one entry in a leaderboard. TODO : Remove inheritance to Run because we don't need all these infos
+    Attributes:
+        place (int) : Place of the object on a specific leaderboard.
+            self.place = 3 means the entry is 3rd on the leaderboard
+        min/rk (float) : Average time to gain 1 rank on the leaderboard
+            min/rk = 5 => In average, the runner need to improve 5 seconds in order to move up in the leaderboard for one place
+                NOTE : equalizing an another run is considered moving up a rank.
+
+        WR time (float) : WR time of the leaderboard, in seconds
+            (TODO : Remove this attribute for Rank)
+        delta WR (float): Difference of the time compared to WR, in seconds
+            delta WR = 5 means the rank is 5 secondes behind WR
+        WR % (float) : % of the time compared to WR
+            self["WR %"] = 2 means the rank is 200% longer than the WR
+            self["WR %"] = 1 means the rank is the WR
+    """
+
+    def __init__(self, data: dict, WR_time: float):
+        """
+        Args:
+            data (dict): Informations received from SRC's api.
+                keys:
+            WR_time (float): WR time of the leaderboard, in seconds
+        """
         super().__init__(data["run"])
         self.place = data["place"]
         self["WR time"] = WR_time
-        self.update_data()
 
-    def update_data(self):
         self["WR %"] = self.time / self["WR time"]
         self["delta WR"] = self.time - self["WR time"]
 
-    def __add__(self, other):
-        tempo = super().__add__(other)
-        tempo["WR %"] = tempo.time / tempo["WR time"]
+        self["min/rk"] = 0
+        if self.place > 1:
+            self["min/rk"] = self["delta WR"] / (self.place - 1)
 
-        return tempo
-
-    def __truediv__(self, other):
-        tempo = super().__truediv__(other)
-        tempo["WR %"] = tempo.time / tempo["WR time"]
-
-        return tempo
-
-
-
-    def __str__(self):  #TODO: use the new attri
-        time_str = lambda x : f'{int(x)//3600:>3}:{int(x) % 3600 // 60:02}:{int(x) % 3600 % 60 % 60:02}'
-        return "   ".join(map(str, [time_str(self.time),f'-{time_str(self.time - self["WR time"]).lstrip()}', f'{self["WR %"]:.2%}']))
+    def __str__(self):
+        time_str = lambda x: f"{int(x)//3600:>3}:{int(x) % 3600 // 60:02}:{int(x) % 3600 % 60 % 60:02}"
+        return "   ".join(map(str,[
+                                    time_str(self.time),
+                                    f'+{time_str(self["delta WR"]).lstrip()}',
+                                    f'({self["WR %"]:.2%})',
+                                    f'{time_str(self["min/rk"]).lstrip()}'
+                                    ]
+                            )
+                        )
