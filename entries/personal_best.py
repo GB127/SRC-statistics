9566de02e43cb6021d5449a746f9233bf796f6c6
Guylain <guylain.breton.1@ulaval.ca>
2022-04-07 10:00:59 -0400

    docstring pb

--- entries/personal_best.py
+++ entries/personal_best.py
@@ -3,7 +3,48 @@ from tables.leaderboard import LB
 from code_SRC.api import api
 
 class PB(Rank):
+    """Class related to one PB on SRC.
+        Attributes:
+            game (str): Game of the speedrun
+                example: Super Mario 64
+
+            level (str or None) : Level of the speedrun, if the run is on a level. If the run is not on a level, the value is None
+                example : Dragon Roost Cavern (a dungeon in Zelda: Wind Waker)
+
+            category (str): Category of the speedrun. String in parenthesis is the subcategory if there is one.
+                example : 120 stars (WII VC)
+
+            emu (bool): True if run is ran on an emulator. False otherwise
+
+            region (str): Region of the game being ran.
+                example : USA / NTSC
+
+            system (str): System of the run.
+                example : Gamecube
+
+            time (float) : Time of the speedrun, in seconds.
+
+            place (int) : Place of the object on a specific leaderboard.
+                self.place = 3 means the entry is 3rd on the leaderboard
+
+        WR time (float) : WR time of the leaderboard, in seconds
+
+        delta WR (float): Difference of the time compared to WR, in seconds
+            delta WR = 5 means the rank is 5 secondes behind WR
+
+        WR % (float) : % of the time compared to WR
+            self["WR %"] = 2 means the rank is 200% longer than the WR
+            self["WR %"] = 1 means the rank is the WR
+
+    """
     def __init__(self, data):
+        """
+
+        Args:
+            data (dict): Informations received from SRC's api.
+                keys:
+        """
+
         category_id = data["run"]["category"]
         game_id = data["run"]["game"]
         sub_cat_ids = {}
@@ -15,11 +56,7 @@ class PB(Rank):
         self.leaderboard = LB(api.leaderboard(game_id, category_id, sub_cat_ids, level_id))
         super().__init__(data, self.leaderboard.WR)
 
-        if isinstance(self.leaderboard, LB):
-            self["LB %"] = (len(self.leaderboard) - self.place)/len(self.leaderboard)
-        elif isinstance(self.leaderboard, (int, float)):
-            self["LB %"] = (self.leaderboard - self.place)/self.leaderboard
-
+        self["LB %"] = (len(self.leaderboard) - self.place)/len(self.leaderboard)
     def __str__(self):
         time_str = lambda x : f'{int(x)//3600:>3}:{int(x) % 3600 // 60:02}:{int(x) % 3600 % 60 % 60:02}'
         inted_lb = self.leaderboard if isinstance(self.leaderboard, (float, int)) else len(self.leaderboard)
