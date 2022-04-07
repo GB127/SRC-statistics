8a5f5e63ecd15f96cb8d5117d0edd581f3ea3d07
Guylain <guylain.breton.1@ulaval.ca>
2022-04-07 10:04:56 -0400

    docstringing

--- tables/leaderboard.py
+++ tables/leaderboard.py
@@ -2,11 +2,23 @@ from entries.lb_entry import Rank
 from tables.base import Base_Table
 
 class LB(Base_Table):
+    """Class representing a leaderboard on SRC.
+
+        Attributes:
+            data (list): List of Rank objects. Classed by place.
+                id 0 == WR
+                id -1 == last place
+            WR (int): WR of the leaderboard. TODO : Consider changing this to a method...
+        """
     def __init__(self, list_Ranks:list):
+        """
+            Args:
+                list_Ranks (list): List of Dictionnaries of the leaderboard received from SRC.
+                    See lb_entry.py for format of dictionnaries.
+            """
         self.data = []
         self.WR = list_Ranks[0]["run"]["times"]["primary_t"]
 
-        #self.WR = list_Ranks[0]["run"]["times"]["primary_t"]
         for data in list_Ranks:
             self.data.append(Rank(data, self.WR))
 
