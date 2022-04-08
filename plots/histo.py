258a901b50078e5a6ad3deb78ee0da88b2caf87f
Guylain <guylain.breton.1@ulaval.ca>
2022-04-08 19:31:57 -0400

    fix the not so cool y label for histo, for once.

--- plots/histo.py
+++ plots/histo.py
@@ -22,15 +22,12 @@ class Histo_app(Base_app):
         to_plot = [x[kargs["number"]] for x in self.data]
         self.canvas.figure.clf()
         self.ax = self.canvas.figure.subplots()
+        self.ax.hist(to_plot, range=(min(to_plot), max(to_plot)))
 
         def labels():
             self.ax.set_title(f'{self.data[0].__class__.__name__} - {"time"}')
             self.ax.set_xlabel(kargs["number"])
             self.ax.set_ylabel("Frequency")
-            self.ax.hist(to_plot, range=(min(to_plot), max(to_plot)))
-
-            self.canvas.draw()
-
 
         def set_xticks():
             self.ax.set_xticks(arange(
@@ -38,13 +35,8 @@ class Histo_app(Base_app):
                                     max(to_plot),
                                     (max(to_plot)-min(to_plot))/5
                                     )[1:])
-            #self.ax.set_yticks(range(0, int(max(self.ax.get_yticks())+1), int(max(self.ax.get_yticks()) // 14 + 1))[1:-1])
-
-            self.canvas.draw()
-
             self.ax.set_xlim([min(to_plot), max(to_plot)])
-            
-            
+
             if kargs["number"] in ["time"]:
                 time_str = lambda x : f'{int(x//3600):>3}:{int(x) % 3600 // 60:02}:{int(x) % 3600 % 60 % 60:02}'
                 self.ax.set_xticklabels([time_str(float(x)) for x in self.ax.get_xticks()], horizontalalignment="center")
@@ -52,11 +44,10 @@ class Histo_app(Base_app):
             elif kargs["number"] in ["WR %"]:
                 perc_str = lambda x : f'{x:.1%}'
                 self.ax.set_xticklabels([perc_str(float(x)) for x in self.ax.get_xticks()], horizontalalignment="center")
-
-
-
-
+            self.canvas.draw()
 
         labels()
         set_xticks()
+
+        self.ax.set_yticks([x for x in range(0, int(max(self.ax.get_yticks()) + 1), int(max(self.ax.get_yticks()) + 1) // 14)][1:])
         self.canvas.draw()
